9a72f6e2a052428fa9502f3051b66d053795bc49
dominic <dominicseduraja3@gmail.com>
2022-09-09 00:44:34 +0530

    seventh commit

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,19 +1,33 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useCallback, useState} from "react";
 import Quill from 'quill';
+import ReactQuill from 'react-quill';
 import "quill/dist/quill.snow.css"
 
 const TextEditor = () => {
 
+    const [value, setValue] = useState('');
     useEffect(()=>{
         const q = new Quill('#editor',{
             theme:'snow'
         });
     },[])
 
+    // const wrapperRef = useCallback((wrapper)=>{
+    //     if(wrapper == null) return;
+    //     wrapper.innerHTML = "";
+    //     const editor = document.createElement("div");
+    //     wrapper.append(editor);
+    //      new Quill(editor,{
+    //         theme:"show",
+    //     });
+    // },[]);
+
     return (
-        <div className="container" id ="editor">
-            
+        <>
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <div className="container" id ="editor" r>
         </div>
+        </>
     )
 }
 
