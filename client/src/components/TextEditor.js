3280bb395a00bbecf3c4d28e4f745616e23a3e05
dominic <dominicseduraja3@gmail.com>
2022-09-12 22:26:32 +0530

    frontend works

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,34 +1,43 @@
-import React,{useEffect, useCallback, useState} from "react";
+import React,{useEffect, useCallback } from "react";
 import Quill from 'quill';
-import ReactQuill from 'react-quill';
 import "quill/dist/quill.snow.css"
+import "./TextEditor.css"
+
+const TOOLBAR_OPTIONS= [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['image', 'blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
 
 const TextEditor = () => {
-
-    const [value, setValue] = useState('');
-    useEffect(()=>{
-        const q = new Quill('#editor',{
-            theme:'snow'
+    const wrapperRef = useCallback((wrapper)=>{
+        if(wrapper == null) return;
+        wrapper.innerHTML = "";
+        const editor = document.createElement("div");
+        wrapper.append(editor);
+         new Quill(editor,{
+            theme:"snow",
+            modules:{
+                toolbar:TOOLBAR_OPTIONS
+            }
         });
-    },[])
+    },[]);
 
-    // const wrapperRef = useCallback((wrapper)=>{
-    //     if(wrapper == null) return;
-    //     wrapper.innerHTML = "";
-    //     const editor = document.createElement("div");
-    //     wrapper.append(editor);
-    //      new Quill(editor,{
-    //         theme:"show",
-    //     });
-    // },[]);
-
-    return (
-        <>
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
-        <div className="container" id ="editor" r>
-        </div>
-        </>
-    )
-}
+    return  <div className="container"  ref={wrapperRef}></div>  
+};
 
 export default TextEditor
